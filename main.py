370f61c4a74515291f06228449f1bb92654ccbdd
Falco90 <falcorodenburg@gmail.com>
2026-09-09 14:16:59 +0700

    change agent.invoke() to agent.ainvoke() and make main async

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -6,12 +7,16 @@ from oao.graph import build_graph
 
 wallet_address = os.environ["WALLET_ADDRESS"]
 
-app = build_graph()
+async def main():
+    app = build_graph()
 
-result = app.invoke(
-    {
-        "wallet_address": wallet_address
-    }
-)
+    result = await app.ainvoke(
+        {
+            "wallet_address": wallet_address
+        }
+    )
 
-print(result)
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
