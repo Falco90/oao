36b68999f3bdd6c70969a0b5b02a5d49cb727ea9
Falco90 <falcorodenburg@gmail.com>
2026-09-13 08:05:58 +0700

    add vercel domain to CORS middleware

--- src/oao/api.py
+++ src/oao/api.py
@@ -27,6 +27,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://oao-agent.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
